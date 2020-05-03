618af40d6df2cbe241bc04b005174be83bfca366
Joey <jmichaelt98@gmail.com>
2020-05-03 11:41:19 -0400

    Add favicon

--- src/components/SiteMetadata.tsx
+++ src/components/SiteMetadata.tsx
@@ -22,6 +22,7 @@ function SiteMetadata(props: Props) {
           content: "width=device-width, initial-scale=1.0",
         },
       ]}
+      link={[{ rel: "shortcut icon", href: "img/favicon.png" }]}
     ></Helmet>
   );
 }
